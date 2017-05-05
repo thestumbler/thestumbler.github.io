0c3c16e88cb42cf9b060ca72a24c2bdb0d3b3913
Chris Lott <rclott@hiwaay.net>
2017-05-05 21:19:00 +0900

    added lab detail post

--- images/orig/conv_one.sh
+++ images/orig/conv_one.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f ../$1 ]
 then
-  convert $1 -resize "800x800>" ../$1
+  convert $1 -auto-orient -resize "800x800>" ../$1
   echo "CONVERTED:  $1"
 else
   echo "skipped:    $1"
