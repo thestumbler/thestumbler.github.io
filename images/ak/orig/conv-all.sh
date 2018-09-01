403b16c671f910fe718ffbbe2fb0c915b69f3518
Chris Lott <rclott@hiwaay.net>
2018-09-01 09:11:30 +0900

    updated alaska project

---
+++ images/ak/orig/conv-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./conv-one.sh bus-drivers-seat.jpg
+./conv-one.sh bus-for-sale-front.jpg
+./conv-one.sh bus-for-sale-inside.jpg
+./conv-one.sh bus-for-sale.jpg
+./conv-one.sh bus-hambaugh-inside.jpg
+./conv-one.sh bus-hambaugh.jpg
+./conv-one.sh conv-lab.sh
+./conv-one.sh conv-one.sh
+./conv-one.sh glennallen-alaska.jpg
+./conv-one.sh house611-back.jpg
+./conv-one.sh house611-front.jpg
+./conv-one.sh map-alaska-state.jpg
+./conv-one.sh map-all.png
+./conv-one.sh map-canada.png
+./conv-one.sh map-usa.png
